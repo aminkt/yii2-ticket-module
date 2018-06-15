a6cc134ba03a216c41a5b970e8890ccac7ea477a
Mohammad_Pvn <mohammad.pvn1375@gmail.com>
2018-06-15 12:35:47 +0430

    done

--- controllers/api/CustomerController.php
+++ controllers/api/CustomerController.php
@@ -46,65 +46,6 @@ class CustomerController extends \yii\rest\Controller
     }
 
     /**
-     * create new ticket
-     *
-     * @return \aminkt\ticket\models\TicketMessage
-     * @throws \Exception
-     * @throws \Throwable
-     *
-     * @author  Mohammad Parvaneh <mohammad.pvn1375@gmail.com>
-     */
-    public function actionCreate()
-    {
-        $ticketId = \Yii::$app->getRequest()->post('ticketId');
-        if ($ticketId) {
-            $model = Ticket::findOne($ticketId);
-            if (!$model) {
-                Alert::error('تیکت پیدا نشد', '');
-            }
-        } else {
-            $customer = \Yii::$app->getUser()->getIdentity();
-            if (!$customer) {
-                $name = \Yii::$app->getRequest()->post('name');
-                $mobile = \Yii::$app->getRequest()->post('mobile');
-                $email = \Yii::$app->getRequest()->post('email');
-                $customer = new CustomerTempModel($name, $email, $mobile);
-            }
-            $category = TicketCategory::findOne(\Yii::$app->getRequest()->post('categoryId'));
-            $subject = \Yii::$app->getRequest()->post('subject');
-            $model = Ticket::createNewTicket($subject, $customer, $category);
-        }
-        $message = \Yii::$app->getRequest()->post('message');
-        $attachment = \Yii::$app->getRequest()->post('attachment');
-        $ticketMessage = $model->sendNewMessage($message, $attachment);
-        return $ticketMessage;
-    }
-
-    /**
-     * close ticket
-     *
-     * @return bool
-     *
-     * @author Mohammad Parvaneh <mohammad.pvn1375@gmail.com>
-     */
-    public function actionClose()
-    {
-        $model = Ticket::findOne(\Yii::$app->getRequest()->get('ticketId'));
-        if (!$model) {
-            Alert::error('تیکت پیدا نشد', '');
-            return false;
-        } else {
-            if ($model->closeTicket()) {
-                Alert::success('تیکت با موفقیت بسته شد', 'اسم تیکت جدید : ' . $model->subject);
-                return true;
-            } else {
-                Alert::success('تیکت بسته نشد', $model->subject);
-                return false;
-            }
-        }
-    }
-
-    /**
      * return user tickets List
      *
      * @return array|\yii\db\ActiveRecord[]
@@ -118,19 +59,5 @@ class CustomerController extends \yii\rest\Controller
         $customer = \Yii::$app->getUser()->getIdentity();
         $ticketList = Ticket::find()->where('customerId' == $customer->getId())->all();
         return $ticketList;
-    }
-
-    /**
-     * return ticket with it's messages by ticket trackingCode
-     *
-     * @return null|static
-     *
-     * @author Mohammad Parvaneh <mohammad.pvn1375@gmail.com>
-     */
-    public function actionView()
-    {
-        $trackingCode = \Yii::$app->getRequest()->get('trackingCode');
-        $ticket = Ticket::findOne(['trackingCode' => $trackingCode]);
-        return $ticket;
     }
 }
