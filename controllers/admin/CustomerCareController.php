e14ed4551ed4a83d60360285898a79afc024b865
Mohammad_Pvn <mohammad.pvn1375@gmail.com>
2018-06-15 12:34:26 +0430

    done

--- controllers/admin/CustomerCareController.php
+++ controllers/admin/CustomerCareController.php
@@ -18,44 +18,4 @@ class CustomerCareController extends Controller
     {
         return $this->render('index');
     }
-
-    /**
-     * Create and update department
-     *
-     * @param null $id
-     *
-     * @return string|\yii\web\Response
-     *
-     * @author Saghar Mojdehi <saghar.mojdehi@gmail.ocm>
-     */
-    public function actionDepartment($id = null)
-    {
-        $dataProvider = new ActiveDataProvider([
-            'query' => Department::find()
-        ]);
-
-        if ($id) {
-            $model = Department::findOne($id);
-            if (!$model) {
-                Alert::error('خطا در انجام عملیات', "دپارتمان مورد نظر یافت نشد");
-                return $this->redirect(['department']);
-            }
-        } else {
-            $model = new Department();
-        }
-
-        if (\Yii::$app->getRequest()->isPost and $model->load(\Yii::$app->request->post())) {
-            if (!$model->save()) {
-                Alert::error('خطا در انجام عملیات', 'دپارتمان ذخیره نشد. دوباره تلاش کنید.');
-            }
-            Alert::success('عملیات با موفقیت انجام شد', '');
-            return $this->redirect(['department']);
-        }
-
-        return $this->render('department', [
-            'dataProvider' => $dataProvider,
-            'model' => $model
-        ]);
-    }
-
 }
--- controllers/api/CustomerController.php
+++ controllers/api/CustomerController.php
@@ -1,39 +1,136 @@
 <?php
-
 namespace aminkt\ticket\controllers\front;
-use aminkt\ticket\models\TicketCategories;
-use aminkt\ticket\models\TicketMessage;
-use aminkt\ticket\models\Tickets;
-use aminkt\ticket\Ticket;
+use aminkt\ticket\models\CustomerTempModel;
+use aminkt\ticket\models\TicketCategory;
+use aminkt\ticket\models\Ticket;
+use yii\data\ActiveDataProvider;
+use yii\filters\AccessControl;
+use yii\filters\auth\HttpBearerAuth;
 use aminkt\widgets\alert\Alert;
-use yii\web\NotFoundHttpException;
-
 /**
  * Class CustomerController
  *
  * @package aminkt\ticket
+ *
+ * @author Mohammad Parvaneh <mohammad.pvn1375@gmail.com>
  */
-class CustomerController extends \yii\web\Controller
+class CustomerController extends \yii\rest\Controller
 {
-    public function actionIndex($id = null)
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
     {
-        if ($id) {
-            $model = Tickets::findOne($id);
-            if (!$model) {
-                throw new NotFoundHttpException("دسته مورر نظر یافت نشد");
-            }
-        } else {
-            $category=TicketCategories::findOne(\Yii::$app->getRequest()->post(['categoryId']));
-            $subject=\Yii::$app->getRequest()->post(['subject']);
-            $model = Tickets::createNewTicket($subject,null,$category);
-        }
+        $behaviors = parent::behaviors();
 
-
-
-        return $this->render('index', [
-            'model' => $model
-        ]);
+        $behaviors['authenticator'] = [
+            'class' => HttpBearerAuth::class,
+            'optional' => ['close', 'create', 'list', 'index', 'view']
+        ];
+        $behaviors['access'] = [
+            'class' => AccessControl::class,
+            'only' => ['index', 'create'],
+            'rules' => [
+                [
+                    'actions' => ['index', 'close', 'view'],
+                    'allow' => true,
+                    'roles' => ['@'],
+                ],
+                [
+                    'actions' => ['create', 'close', 'view'],
+                    'allow' => true,
+                ]
+            ],
+        ];
+        return $behaviors;
     }
 
+    /**
+     * create new ticket
+     *
+     * @return \aminkt\ticket\models\TicketMessage
+     * @throws \Exception
+     * @throws \Throwable
+     *
+     * @author  Mohammad Parvaneh <mohammad.pvn1375@gmail.com>
+     */
+    public function actionCreate()
+    {
+        $ticketId = \Yii::$app->getRequest()->post('ticketId');
+        if ($ticketId) {
+            $model = Ticket::findOne($ticketId);
+            if (!$model) {
+                Alert::error('تیکت پیدا نشد', '');
+            }
+        } else {
+            $customer = \Yii::$app->getUser()->getIdentity();
+            if (!$customer) {
+                $name = \Yii::$app->getRequest()->post('name');
+                $mobile = \Yii::$app->getRequest()->post('mobile');
+                $email = \Yii::$app->getRequest()->post('email');
+                $customer = new CustomerTempModel($name, $email, $mobile);
+            }
+            $category = TicketCategory::findOne(\Yii::$app->getRequest()->post('categoryId'));
+            $subject = \Yii::$app->getRequest()->post('subject');
+            $model = Ticket::createNewTicket($subject, $customer, $category);
+        }
+        $message = \Yii::$app->getRequest()->post('message');
+        $attachment = \Yii::$app->getRequest()->post('attachment');
+        $ticketMessage = $model->sendNewMessage($message, $attachment);
+        return $ticketMessage;
+    }
 
+    /**
+     * close ticket
+     *
+     * @return bool
+     *
+     * @author Mohammad Parvaneh <mohammad.pvn1375@gmail.com>
+     */
+    public function actionClose()
+    {
+        $model = Ticket::findOne(\Yii::$app->getRequest()->get('ticketId'));
+        if (!$model) {
+            Alert::error('تیکت پیدا نشد', '');
+            return false;
+        } else {
+            if ($model->closeTicket()) {
+                Alert::success('تیکت با موفقیت بسته شد', 'اسم تیکت جدید : ' . $model->subject);
+                return true;
+            } else {
+                Alert::success('تیکت بسته نشد', $model->subject);
+                return false;
+            }
+        }
+    }
+
+    /**
+     * return user tickets List
+     *
+     * @return array|\yii\db\ActiveRecord[]
+     * @throws \Exception
+     * @throws \Throwable
+     *
+     * @author Mohammad Parvaneh <mohammad.pvn1375@gmail.com>
+     */
+    public function actionList()
+    {
+        $customer = \Yii::$app->getUser()->getIdentity();
+        $ticketList = Ticket::find()->where('customerId' == $customer->getId())->all();
+        return $ticketList;
+    }
+
+    /**
+     * return ticket with it's messages by ticket trackingCode
+     *
+     * @return null|static
+     *
+     * @author Mohammad Parvaneh <mohammad.pvn1375@gmail.com>
+     */
+    public function actionView()
+    {
+        $trackingCode = \Yii::$app->getRequest()->get('trackingCode');
+        $ticket = Ticket::findOne(['trackingCode' => $trackingCode]);
+        return $ticket;
+    }
 }
